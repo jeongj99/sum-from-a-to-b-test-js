f84e3a3ae5e8de99cf2f0f7d6c740814f4d2c96b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-02 21:51:47 +0000

    Implemented a the sum from a to b using recursion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = (fromN, toN) => {
+  if (fromN === toN) {
+    return fromN;
+  }
+  return fromN + sum(fromN + 1, toN);
+};
 
 module.exports = sum;
